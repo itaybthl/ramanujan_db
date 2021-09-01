62d7aceb136f6542488ed612fdf6f46a1e2fd023
Amir Klein <amirkl94@gmail.com>
2021-09-01 11:16:28 +0300

    [jobs]: Add summary job
    This job will run once per-day and send an email with a report of the
    findings that were made during that day.

---
+++ jobs/job_day_summary.py
@@ -0,0 +1,103 @@
+from db import models
+from db import ramanujan_db
+from datetime import date,timedelta
+import xlsxwriter as xl
+import yagmail
+
+sender_email = "ramanumachproj@gmail.com"
+receivers_email  = ["yoni420@gmail.com","amirkl94@gmail.com"]
+password = "Ramanujan123" # for documantation not used in the code
+
+
+db_handle = ramanujan_db.RamanujanDB()
+
+def getLastResults(model):
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    return db_handle.session.query(model).filter(model.insertion_date > yesterday)
+
+def write_const_results(sheet, results):
+    sheet.write('A1','const symbol')
+    sheet.write('B1','const description')
+    sheet.write('C1','cf numerator')
+    sheet.write('D1','cf denominator')
+    sheet.write('E1','connection type')
+    sheet.write('F1','connection details')
+
+    i=1
+    for result in results:
+        constant = db_handle.session.query(models.Constant).filter(models.Constant.constant_id == result.constant_id).one()
+        cf = db_handle.session.query(models.Cf).filter(models.Cf.cf_id == result.cf_id).one()
+        assert(constant)
+        assert(cf)
+        sheet.write(i,0, constant.name)
+        sheet.write(i,1, constant.description)
+        sheet.write(i,2, cf.partial_numerator)
+        sheet.write(i,3, cf.partial_denominator)
+        sheet.write(i,4, result.connection_type)
+        sheet.write(i,5, result.connection_details)
+        i+=1    
+   
+
+def write_cf_results(sheet, results):
+    sheet.write('A1','source cf numenator')
+    sheet.write('B1','source cf denominator')
+    sheet.write('C1','target cf numenator')
+    sheet.write('D1','target cf denominator')
+    sheet.write('E1','connection type')
+    sheet.write('F1','connection details')
+
+    i=1
+    for result in results:
+        source_cf = db_handle.session.query(models.Cf).filter(models.Cf.cf_id == result.source_cf).one()
+        target_cf = db_handle.session.query(models.Cf).filter(models.Cf.cf_id == result.target_cf).one()
+        assert(source_cf)
+        assert(target_cf)
+        sheet.write(i,0, source_cf.partial_numerator)
+        sheet.write(i,1, source_cf.partial_denominator)
+        sheet.write(i,2, target_cf.partial_numerator)
+        sheet.write(i,3, target_cf.partial_denominator)
+        sheet.write(i,4, result.connection_type)
+        sheet.write(i,5, result.connection_details)
+        i+=1    
+
+
+def send_summary(summary,xsl_file_name):
+
+    yag = yagmail.SMTP(sender_email)
+    for receiver in receivers_email:
+        yag.send(
+        to=receiver,
+        subject="A day Summary from the Online Ramanujan machine",
+        contents=summary, 
+        attachments=xsl_file_name,
+        )
+
+def run_job(): 
+    constants_results = getLastResults(models.CfConstantConnection)
+    cf_results = getLastResults(models.ContinuedFractionRelation)
+
+    today_str = date.today().strftime("%d/%m/%Y")
+
+    today_summary_excel = "summary "+ date.today().strftime("%d_%m_%Y")+ ".xslx"
+
+    workbook = xl.Workbook(today_summary_excel)
+    if len(constants_results) > 0:
+        const_sheet = workbook.add_worksheet("constants results")
+        write_const_results(const_sheet,constants_results)
+        
+    if len(cf_results) > 0:
+        cf_sheet = workbook.add_worksheet("cf results")
+        write_cf_results(cf_sheet,cf_results)
+
+    summary = "summary results for " + today_str + "\n"
+    summary += "found total of " + str(len(constants_results)) + " constants relations.\n"
+    summary += "found total of " + str(len(cf_results)) + " cf relations.\n"
+
+    workbook.close()
+
+    send_summary(summary,today_summary_excel)
+
+    #TODO: maybe put all excels results in a designated folder
+
+
