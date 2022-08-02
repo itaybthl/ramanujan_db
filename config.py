67f5d219ddb8d5eb39390f6218a7e17721976c2e
TheMightyDozen <30323792+itaybthl@users.noreply.github.com>
2022-08-02 11:34:41 +0300

    some extra documentation first

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+# how this works:
+# Every job is run (at most) 'iterations' amount of times, defaults to infinity.
+# Every name must correspond to a file in jobs folder of the format 'job_*.py'.
+# In each file, the simpler case is when it doesn't have the function 'run_query', and then it runs 'execute_job' with 'args' and finishes.
+# Otherwise it runs 'run_query' with 'args' and then 'execute_job' with its results, and finally 'summarize_results' with the results of that.
+# In the latter mode, the results of 'execute_job' can be piped back into additional 'execute_job' calls of the same job via "subjobs".
 configuration = {
         'pool_size': 10,
         'jobs_to_run': {
@@ -22,6 +28,7 @@ configuration = {
             }
         }
 
+# If you make your own database, 'name' must match the name in 'create_db.sql' in the line 'CREATE DATABASE <name>'
 db_configuration = {
         'host': '',
         'port': 5432,
