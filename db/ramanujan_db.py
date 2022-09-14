179a6fcfb4e5ae6f222da381f05a034ca047ea04
itaybthl <30323792+itaybthl@users.noreply.github.com>
2022-09-14 19:07:44 +0300

    integrating the new pcf code
    insert_pcf_into_db was integrated into ramanujan_db itself
    also write_constants got trimmed down of its old code and the remaining code was absorbed into create_db

--- db/ramanujan_db.py
+++ db/ramanujan_db.py
@@ -1,9 +1,23 @@
 import logging
 import config
-from sqlalchemy.orm import sessionmaker
+import mpmath
 from . import models
+import sympy
+from sympy import Poly
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import sessionmaker
+from psycopg2.errors import UniqueViolation
+from pcf.PCF import PCF
+from typing import Tuple, List, Dict
+CanonicalForm = Tuple[List[int], List[int]]
+
+# TODO: fix this (the import of ramanujan doesn't seem to work)
+
+# from ramanujan.enumerators.FREnumerator import check_for_fr
+# from ramanujan.enumerators.FREnumerator import FIRST_ENUMERATION_MAX_DEPTH as MAX_ITERATOR_STEPS
+MAX_ITERATOR_STEPS = 1402
 
 class RamanujanDB(object):
     def __init__(self):
@@ -15,18 +29,126 @@ class RamanujanDB(object):
 
     @property
     def constants(self):
-        return self.session.query(models.Constant).order_by(models.Constant.constant_id)
+        return self.session.query(models.Constant).order_by(models.Constant.const_id)
 
     @property
     def cfs(self):
         return self.session.query(models.PcfCanonicalConstant).order_by(models.PcfCanonicalConstant.const_id)
 
-    def add_cfs(self, cf_list, conflict=False):
-        if not conflict:
-            self.session.add_all(cf_list)
+    def add_pcf_canonical(self, canonical_form_numerator: List[int], canonical_form_denominator: List[int]) -> None:
+        # TODO implement add_pcf_canonicals that uploads multiple at a time
+        pcf = models.PcfCanonicalConstant()
+        pcf.base = models.Constant()
+        pcf.p = canonical_form_numerator
+        pcf.q = canonical_form_denominator
+        self.session.add(pcf)
+        
+        # yes, commit and check error is better than preemptively checking if unique and then adding,
+        # since the latter is two SQL commands instead of one, which breaks on "multithreading" for example
+        # and also should be generally slower
+        # also this can't be turned into a kind of INSERT pcf ON CONFLICT DO NOTHING statement
+        # since this needs the base Constant to be added first so it gains its const_id...
+        # TODO investigate if triggers can make something like ON CONFLICT DO NOTHING work anyway,
+        # possibly will help with the previous TODO... maybe something like:  https://stackoverflow.com/questions/46105982/postgres-trigger-function-on-conflict-update-another-table
+        try:
             self.session.commit()
-        else:
-            insert_statement = postgresql.insert(models.Cf).values(cf_list)
-            self._engine.execute(insert_statement.on_conflict_do_nothing())
+        except Exception as e:
+            self.session.rollback()
+            raise e
+
+    class NoFRException(Exception):
+        pass
+    
+    def add_pcf(self, pcf: PCF, check_fr: bool = True) -> None:
+        """
+        Expect PCF object.
+        raises IntegrityError if pcf already exists in the db.
+        """
+        if check_fr:
+            with mpmath.workdps(20_000):
+                pass
+                # TODO: fix this
+                # an_iterator = poly_to_iterator(pcf.an)
+                # bn_iterator = poly_to_iterator(pcf.bn)
+                # an_degree = pcf.an.degree()
+                # if not check_for_fr(an_iterator, bn_iterator, an_degree)[0]:
+                #     raise self.NoFRException()
+        top, bot = pcf.get_canonical_form()
+        # By default the coefs are of type sympy.core.numbers.Integer but sql need them to be integers
+        self.add_pcf_canonical([int(coef) for coef in top.all_coeffs()], [int(coef) for coef in bot.all_coeffs()])
+    
+    def add_pcfs(self, pcfs: List[PCF]) -> Tuple[List[PCF], Dict[str, List[PCF]]]:
+        """
+        Expects a list of PCF objects.
+        """
+        successful = []
+        unsuccessful = {"Already exist": [], "No FR": []}
+        for pcf in pcfs:
+            try:
+                self.add_pcf(pcf)
+                successful.append(pcf)
+            except IntegrityError as e:
+                if not isinstance(e.orig, UniqueViolation):
+                    raise e # otherwise already in the DB
+                unsuccessful["Already exist"].append(pcf)
+            except self.NoFRException:
+                unsuccessful["No FR"].append(pcf)
+        return successful, unsuccessful
+    
+    def add_pcfs_silent(self, pcfs: List[PCF]) -> None:
+        """
+        Expects a list of PCF objects. Doesn't return which PCFs were successfully or unsuccessfully added.
+        """
+        for pcf in pcfs:
+            try:
+                self.add_pcf(pcf)
+            except IntegrityError as e:
+                if not isinstance(e.orig, UniqueViolation):
+                    raise e # otherwise already in the DB
+            except self.NoFRException:
+                pass
+    
+    @staticmethod
+    def parse_cf_to_lists(cf: models.PcfCanonicalConstant) -> CanonicalForm:
+        return [int(coef) for coef in cf.p], [int(coef) for coef in cf.q]
+    
+    @staticmethod
+    def canonical_form_to_pcf(canonical_form: CanonicalForm) -> PCF:
+        """
+        Receive the canonical form of a pcf (an := 1 ; bn := bn / (an*a(n+1)))
+        and return a pcf of this canonical form.
+        Notice there may be many pcfs that fit the same canonical form, this returns just one of them.
+        TODO: add link to the doc which explains this
+        """
+        n = sympy.Symbol("n")
+        an = Poly(canonical_form[1], n).compose(Poly(n + 1))
+        bn = Poly(canonical_form[0], n) * an
+        pcf = PCF(an.all_coeffs(), bn.all_coeffs())
+        pcf.deflate()
+        return pcf
+    
+    def get_canonical_forms(self) -> List[CanonicalForm]:
+        return [parse_cf_to_lists(pcf) for pcf in self.cfs.all()]
+    
+    def get_actual_pcfs(self) -> List[PCF]:
+        """
+        return a list of PCFs
+        """
+        return [canonical_form_to_pcf(c) for c in self.get_canonical_forms()]
+    
+    @staticmethod
+    def poly_to_iterator(poly):
+        def iterator(poly, max_runs, start_n=0):
+            # be careful before setting start_n!=0
+            for i in range(start_n, max_runs):
+                yield int(poly.eval(i))
+        return iterator(poly, MAX_ITERATOR_STEPS)
 
 
+def main():
+    [print(pcf) for pcf in get_actual_pcfs_from_db()]
+    print("aa")
+
+
+if __name__ == "__main__":
+    main()
--- pcf/PCF.py
+++ pcf/PCF.py
@@ -1,103 +1,85 @@
-import numpy as np
-import math
-
+from __future__ import annotations
 import sympy
 from sympy import *
-from sympy import Poly
+from typing import List
 
 n = sympy.Symbol("n")
 
 
 class PCF:
 
-    def __init__(self, an_coefficients, bn_coefficients, value=None, c_top=None, c_bot=None, precision=0):
+    def __init__(self: PCF, an_coefficients: List[int], bn_coefficients: List[int], value=None, c_top=None, c_bot=None, precision=0):
         """
         an_coefficients, bn_coefficients: lists of integers from the largest power to the smallest power.
         """
-        # Important, if we assign [] in the def row, it will use the same array for every object.
-        if c_bot is None:
-            c_bot = []
-        if c_top is None:
-            c_top = []
         self.an = Poly(an_coefficients, n)
         self.bn = Poly(bn_coefficients, n)
         self.value = value
-        self.c_top = c_top
-        self.c_bot = c_bot
+        # Important, if we assign [] in the def row, it will use the same array for every object.
+        self.c_top = c_top if c_top else []
+        self.c_bot = c_bot if c_bot else []
         self.precision = precision
 
-    def moving_canonical_form(self):
+    def moving_canonical_form(self: PCF):
         # Should always be real roots. (If we find out this isn't the case then we need to modify this)
         bn_roots = self.bn.all_roots()
 
         # In case bn is a constant (has no roots) we still want an to have the canonical roots
         # => (the smallest root to be in (-1,0] )
-        if 0 == len(bn_roots):
+        if len(bn_roots) == 0:
             an_real_roots = self.an.real_roots()
             an_rational_roots = self.an.ground_roots()
 
             # If some of the roots are irrational, it makes the coefficients look ugly, so I decided not to move them.
             # an_rational_roots is a dict {root:power_of_root} while an_real_roots is a list of all of the roots,
             # including multiple repeats.
-            if 0 == len(an_real_roots) or len(an_real_roots) != sum(an_rational_roots.values()):
+            if len(an_real_roots) == 0 or len(an_real_roots) != sum(an_rational_roots.values()):
                 return self.an, self.bn
 
-            an_real_roots.sort()
-            largest_root = an_real_roots[-1]
+            largest_root = max(an_real_roots)
 
         else:
-            bn_roots.sort()
-            largest_root = bn_roots[-1]
+            largest_root = max(bn_roots)
         # We want the largest root to be in (-1,0].
-        bn_poly_new = self.bn.compose(Poly(n + largest_root))
-        an_poly_new = self.an.compose(Poly(n + largest_root))
-        return bn_poly_new, an_poly_new
+        return self.bn.compose(Poly(n + largest_root)), self.an.compose(Poly(n + largest_root))
 
-
-    def inflating_canonical_form(self):
+    def inflating_canonical_form(self: PCF):
         top = self.bn
         bot = self.an * self.an.compose(Poly(n-1))
         gcd = sympy.gcd(top, bot)
-        top_reduced, bot_reduced = cancel(top/gcd), cancel(bot/gcd)
-        return Poly(top_reduced, n), Poly(bot_reduced, n)
+        return Poly(cancel(top/gcd), n), Poly(cancel(bot/gcd), n)
 
-
-    def get_cannonical_form(self):
+    def get_canonical_form(self: PCF):
         top, bot = self.inflating_canonical_form()
-        inflated_pcf = PCF(bot.all_coeffs(), top.all_coeffs())
-        return inflated_pcf.moving_canonical_form()
+        return PCF(bot.all_coeffs(), top.all_coeffs()).moving_canonical_form()
 
-    def get_cannonical_form_string(self):
-        an, bn = self.get_cannonical_form()
+    def get_canonical_form_string(self: PCF):
+        an, bn = self.get_canonical_form()
         return str(bn / an)
 
-    def __str__(self):
-        return "an: " + str(self.an.all_coeffs()) + "\t|\tbn: " + str(self.bn.all_coeffs())
+    def __str__(self: PCF):
+        return f'an: {self.an.all_coeffs()}\t|\tbn: {self.bn.all_coeffs()}'
 
-    def is_inflation(self):
-        top = self.bn
-        bot = self.an * self.an.compose(Poly(n - 1))
-        gcd = sympy.gcd(top, bot)
-        if gcd == 1:
-            return False
-        else:
-            return True
+    def is_inflation(self: PCF):
+        return sympy.gcd(self.bn, self.an * self.an.compose(Poly(n - 1))) != 1
 
-    def deflate(self):
+    def deflate(self: PCF):
         for i in range(self.an.degree()):
+            deflated: bool = False
             an_factors = [factor_tuple[0] for factor_tuple in self.an.factor_list()[1]]
             bn_factors = [factor_tuple[0] for factor_tuple in self.bn.factor_list()[1]]
             for factor in an_factors:
                 if factor in bn_factors and factor.compose(Poly(n-1)) in bn_factors:
                     self.an = Poly(cancel(self.an/factor))
                     self.bn = Poly(cancel(self.bn/(factor * factor.compose(Poly(n-1)))))
-                    continue
+                    deflated = True
+            if not deflated:
+                break # done! nothing's going to change anymore!
 
 
 if __name__ == "__main__":
     bn = sympy.Poly(list([-1, 14, -84, 280, -560, 672, -448, 128, 0]), n)
-    aan = sympy.Poly([4, -56, 360, -1384, 3476, -5844, 6414, -4170, 1215], n)
-    mypcf = PCF(aan.all_coeffs(), (bn * aan.compose(sympy.Poly(n + 1))).all_coeffs())
+    an = sympy.Poly([4, -56, 360, -1384, 3476, -5844, 6414, -4170, 1215], n)
+    mypcf = PCF(aan.all_coeffs(), (bn * an.compose(sympy.Poly(n + 1))).all_coeffs())
     mypcf.deflate()
     mypcf.moving_canonical_form()
-    pass
