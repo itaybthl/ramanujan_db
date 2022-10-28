188bdec4276f414d35298cd6aba720408e88ff0c
eyalk <eyal.kalman@gmail.com>
2022-10-28 19:26:46 +0300

    getting user and password from argument

--- config.py
+++ config.py
@@ -40,15 +40,21 @@ configuration = {
 
 # If you make your own database, 'name' must match the name in 'create_db.sql' in the line 'CREATE DATABASE <name>'
 db_configuration = {
-    'host': '',
+    'host': 'database-1.c1keieal025m.us-east-2.rds.amazonaws.com',
     'port': 5432,
     'user': '',
     'passwd': '',
-    'name': ''
+    'name': 'ramanujanv3'
 }
 
 def get_connection_string(db_name=None):
+    import sys
     conf = db_configuration.copy()
+    if len(sys.argv) >= 2:
+        user_pass = sys.argv[1]
+        username, password = user_pass.split(",")
+        conf["user"] = username
+        conf["passwd"] = password
     if db_name:
         conf['name'] = db_name
     return 'postgresql://{user}:{passwd}@{host}:{port}/{name}'.format(**conf)
